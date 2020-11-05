068d1c06fbadc4460e42ff228b2a660124a121cf
Ben Wiedeback <66647604+Inlic@users.noreply.github.com>
2020-11-05 16:06:56 -0700

    updated auth config for fork

--- client/app/AuthConfig.js
+++ client/app/AuthConfig.js
@@ -1,4 +1,4 @@
 
-export const domain = 'dev-naeu7-5y.us.auth0.com'
-export const clientId = 'X2o65Z6P6BQznnaZdkizPV9bWZPGxulS'
-export const audience = 'https://game-forum.com' 
+export const domain = 'forestlogin223.us.auth0.com'
+export const clientId = 't9ZTG6p58iyE5ncgtnpMQv65PT3QKsQE'
+export const audience = 'https://AuthCheckApi.com' 
